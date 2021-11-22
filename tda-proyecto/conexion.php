7afaca2123bb063c7f65afb0bca3b1897bb150f9
Daniela Montserrat Enriquez Ballesteros <82247636+HatshepsutS@users.noreply.github.com>
2021-11-21 23:32:56 -0600

    Update conexion.php

--- tda-proyecto/conexion.php
+++ tda-proyecto/conexion.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hola mundo"; 
+
 $server='25.84.113.73';
 $user='root';
 $pass='=pEkL8AjixedeYAw6JA@';
@@ -7,6 +7,9 @@ $db='encuesta';
 $conexion=new mysqli($server,$user,$pass,$db);
 if($conexion->connect_errno){
 die("La conexion ha fallado ".$conexion->connect_errno);
+}
+else {
+    echo "Hola mundo"; 
 }
 
 ?>
