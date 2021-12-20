1d387c91e5ed65c81f0b99061fa15e61ef56a786
Melody290 <n.naomi290@gmail.com>
2021-12-20 12:55:06 -0600

    ya nani

---
+++ yadiossueltame.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_POST['submit'])){
+        $secret = "6Ld71bYdAAAAAHE_KZMk70fG7P3m10GXjrWg7aFd";
+        $response = $_POST['g-recaptcha-response'];
+        $remoteip = $_SERVER['REMOTE_ADDR'];
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=&remoteip=$remoteip";
+        $data = file_get_contents($url);
+        $row = json_decode($data,true);
+    }
+?>
