8c5cfecdc0d6b24f30c52e5017f629e657d032b9
Daniela Montserrat Enriquez Ballesteros <82247636+HatshepsutS@users.noreply.github.com>
2022-03-25 19:13:24 -0600

    Delete conexion.php

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server='25.84.113.73';
-$user='root';
-$pass='=pEkL8AjixedeYAw6JA@';
-$db='encuesta';
-$conexion=new mysqli($server,$user,$pass,$db);
-mysqli_set_charset($conexion, "utf8");
-
-?>
